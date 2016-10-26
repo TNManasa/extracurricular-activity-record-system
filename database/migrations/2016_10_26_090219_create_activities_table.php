e619fe710818cf940063efe58da954b83b7f74e6
Menuka <=>
2016-10-26 15:12:39 +0530

    Basic Migrations

---
+++ database/migrations/2016_10_26_090219_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->boolean('validated')->default(0);
+            $table->string('description');
+            // start date, end date, validated, description
+            $table->timestamps();
+        });
+    }
+
+   /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
---
+++ database/migrations/2016_10_26_090245_create_societies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocietiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('societies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('activity_id')->unsigned();
+            $table->string('title');
+            $table->string('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('societies');
+    }
+}
