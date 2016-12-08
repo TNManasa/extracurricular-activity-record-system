e336255e76f744ee603f76a6c855d9e46f4dd702
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2016-12-09 01:12:53 +0530

    Only comments added

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -25,20 +25,19 @@ class StudentsController extends Controller
         $email= $request['email'];
         $role= 'student';
 
-
+        //to update user table
         $line1='insert into users (email,password,role) values ("'.$email.'","'.$pwd.'","'.$role.'")';
         DB::insert($line1);
 
+        //to fetch id, because there is no other way to get that foreign key
         $id1 = DB::select('select id from users where email = ?',[$email]);
         $id=null;
         foreach ($id1 as $user) {
             $id= $user->id;
         }
 
-
+        //to update student table
         $line2= 'insert into students (index_no, first_name, last_name,gender,faculty,id,dob) values ("'.$index_no.'","'.$first_name.'","'.$last_name.'","'.$gender.'","'.$faculty.'","'.$id.'","'.$dob.'")';
-
-
         DB::insert($line2);
         return 'success';
     }
