c31f2aefc73d094481385bff0eca97419d084fab
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2016-12-15 14:58:57 +0530

    added missing batch field in studentController

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -29,7 +29,7 @@ class StudentsController extends Controller
         $last_name=$request['last_name'];
         $dob=$request['dob'];
         $gender=$request['gender'];
-        $faculty=$request['faculty'];
+        $batch=$request['batch'];
         $pwd=Crypt::encrypt($request['password']);
         $email= $request['email'];
         $role= 'student';
@@ -38,7 +38,7 @@ class StudentsController extends Controller
         //becoz , first one is not the best practice
 //        $line1="insert into users (email,password,role) values ('$email','$pwd','$role')";
         $line1="insert into users (email,password,role) values (?,?,?)";
-        DB::insert($line1,[$email,$pwd,$role]);
+        DB::statement($line1,[$email,$pwd,$role]);
 
         //to fetch id, because there is no other way to get that foreign key
         $id1 = DB::select('select id from users where email = ?',[$email]);
@@ -52,8 +52,8 @@ class StudentsController extends Controller
         //first one is  created by concatanating, abd it is not the best practice, Thats y second one
         //$line2= "insert into students (index_no, first_name, last_name,gender,faculty,user_id,dob) values ('$index_no','$first_name','$last_name','$gender','$faculty','$id','$dob')";
 
-        $line2= "insert into students (index_no, first_name, last_name,gender,user_id,dob) values (?,?,?,?,?,?)";
-        DB::insert($line2,[$index_no,$first_name,$last_name,$gender,$id,$dob]);
+        $line2= "insert into students (index_no, first_name, last_name,gender,user_id,dob,batch) values (?,?,?,?,?,?,?)";
+        DB::insert($line2,[$index_no,$first_name,$last_name,$gender,$id,$dob,$batch]);
 
         return 'success';
     }
--- app/User.php
+++ app/User.php
@@ -3,27 +3,79 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class User extends Authenticatable
+class User implements Authenticatable
 {
+    public $id;
+    public $email;
+    public $password;
+    public $role;
+    public $_token;
+
+
+
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * Get the name of the unique identifier for the user.
      *
-     * @var array
+     * @return string
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function getAuthIdentifierName()
+    {
+        return "email";
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->_token;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        // TODO: Implement getRememberTokenName() method.
+    }
 }
