92d66ef843d5600e8426a87dc1e9dee4a6d2720b
Menuka <menuka.14@cse.mrt.ac.lk>
2016-12-20 00:53:57 +0530

    Does not work

---
+++ resources/views/reports/graphs_sports.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title')
+    Bar Charts | Sports | Admin | ECAM
+@stop
+
+@section('content')
+    <div id="chart_div"></div>
+    // With Lava class alias
+    <?= Lava::render('DonutChart', 'IMDB', 'chart-div') ?>
+
+@stop
