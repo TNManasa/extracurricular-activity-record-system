90ef7ff5f25bcad96b978162fb1bfb6ab9a2411a
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2016-12-20 19:18:52 +0530

    Before pulling

---
+++ app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+            $id=Auth::id();
+            $qry=DB::select('select * from students where user_id = ?',[$id]);
+            if($qry==null){
+                return redirect()->back();
+            }else{
+                return $next($request);
+            }
+
+        }else{
+            return redirect()->route('/login');
+        }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 Route::get('/', function () {
     return view('welcome');
-})->name('welcome');
+})->name('welcome')->middleware('auth');
 /*
  * Convention for naming the routes: use lowercase letters, hyphens, periods, Use Plurals
  * Convention for naming Controller methods: Use camelCase
@@ -181,7 +181,7 @@ Route::get('new-activity', [
     // add new activity form
     'uses' => 'ActivitiesController@getNewActivityForm',
     'as' => 'activities.new-activity'
-]);
+])->middleware('auth', 'checkStudent');
 
 Route::post('continue-to-new-activity', [
     // continuing to add new activity after selecting the type
