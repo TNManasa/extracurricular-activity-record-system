3b07603e7ff7023d2757dfcd506c8af8a91581c7
Gathika <gathika.14@cse.mrt.ac.lk>
2016-12-21 21:49:41 +0530

    new view complete_org_activities created

--- sqlFunctions/supervisorViews.sql
+++ sqlFunctions/supervisorViews.sql
@@ -1,1 +1,3 @@
 CREATE VIEW student_activity AS select first_name,last_name,id,student_id,activity_type,start_date,end_date,effort,description,image from students RIGHT JOIN activities on students.index_no=activities.student_id;
+
+CREATE VIEW complete_org_activities AS select organizations.id,organization_name,project_name,role from organizations RIGHT JOIN org_activities on organizations.id=org_activities.org_id;
